eb2047063c41b44eb78d3caccc1b4b57bd7b79d0
Jason Zhou <zhouru@mulgrave.com>
2023-09-23 14:17:49 -0700

    Version 2: Added arrays to J3 solution

--- Sept_16_23/J3.cpp
+++ Sept_16_23/J3.cpp
@@ -2,41 +2,12 @@
 
 using namespace std;
 
-void dayc (int &dn, string ans, int num) {
-
-	if (ans[num] == 'Y'){
-		dn++;
-	}
-
-	else {
-	}
-
-
-
-}
-
-void output (int dayn, int dn, int j, int &counter){
-
-	if (dn == j){
-		if (counter > 0){
-			cout << ",";
-		}
-		cout << dayn;
-		counter++;
-	}
-
-}
-
 int main () {
 
 
 	int N;
-	int d1 = 0;
-	int d2 = 0;
-	int d3 = 0;
-	int d4 = 0;
-	int d5 = 0;
-	int counter = 0;
+	int dn[] = {0,0,0,0,0};
+	int max = 0;
 	string ans;
 
 	cin >> N;
@@ -44,29 +15,32 @@ int main () {
 	for (int i=0; i<N; i++){
 		cin >> ans;
 
-		dayc (d1, ans, 0);
-		dayc (d2, ans, 1);
-		dayc (d3, ans, 2);
-		dayc (d4, ans, 3);
-		dayc (d5, ans, 4);
-
-	}
-
-	for (int j=N; j>0; j--){
-
-		output(1, d1, j, counter);
-		output(2, d2, j, counter);
-		output(3, d3, j, counter);
-		output(4, d4, j, counter);
-		output(5, d5, j, counter);
-
-		if (counter > 0){
-			cout << endl;
-			break;
+		for (int k=0; k<5; k++){
+			if (ans[k] == 'Y'){
+				dn[k]++;
+			}
 		}
 
 	}
 
-		
+
+	for (int m=0; m<5; m++) {
+		if (dn[m] > max){
+			max =dn[m];
+		}
+	}
+
+	int c = 0;
+	
+	for (int j=0; j<5; j++){
+		if (dn[j] == max){
+			if (c>0){
+				cout << ",";
+			}
+			cout << j+1;
+		   	c++;
+		}
+	}
+
 
 }
